7ee8b6d885113c0dbae5bd32b709c0d99a33e6c6
antoniogiordano <antonio.giordano@getapper.com>
2017-05-12 19:57:42 +0200

    Npmignore added

--- src/index.js
+++ src/index.js
@@ -2,6 +2,6 @@
  * Created by antoniogiordano on 12/05/17.
  */
 
-import TextFieldValidated from 'lib/TextFieldValidated/text-field-validated.js'
+import TextFieldValidated from './TextFieldValidated/text-field-validated.js'
 
 export {TextFieldValidated as TextFieldValidated}
