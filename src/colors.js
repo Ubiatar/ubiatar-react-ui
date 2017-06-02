b828d618f537256eb9bd5f6602b6035a95af8ef8
antoniogiordano <antonio.giordano@getapper.com>
2017-06-02 16:21:14 +0200

    Added colors

---
+++ src/colors.js
@@ -0,0 +1,13 @@
+/**
+ * Created by antoniogiordano on 02/06/17.
+ */
+
+const colors = {
+  brand: '#379ACE',
+  darkBrand: '#2B2A3B',
+  success: '#53A182',
+  alert: '#EBCB1A',
+  error: '#EA656C',
+}
+
+export default colors
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@
 
 import TextFieldValidated from './TextFieldValidated/text-field-validated.js'
 import ValidationWrapper from './ValidationWrapper/validation-wrapper.js'
+import colors from './colors.js'
 
 export {TextFieldValidated as TextFieldValidated}
 export {ValidationWrapper as ValidationWrapper}
+export {colors as colors}
